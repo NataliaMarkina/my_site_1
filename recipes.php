239df963783b45db62da989c143098b7d8bce1a5
NataliaMarkina <natalia.marckina2010@yandex.ru>
2019-11-01 23:28:36 +0300

    my_site_1

---
+++ recipes.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Рецепты</title>
+</head>
+<body>
+    <h1>Рецепты</h1>
+
+    <aside>
+        <a href="../species.php">Разновидности</a><br>
+        <a href="../recipes.php">Рецепты</a><br>
+        <a href="../index.html">На главную</a><br><br><br>
+    </aside>
+         
+    <div class="content">
+        <h2>Классический рецепт капкейков</h2>
+        <center><video width="50%" height="48%" controls="controls">
+            <source src="video/video1.mp4" type="video/mp4"> 
+        </video></center>
+        <h2>Капкейки со спрятанным сердцем</h2>
+        <center><video width="50%" height="48%" controls="controls">
+            <source src="video/video2.mp4" type="video/mp4"> 
+        </video></center>
+    </div>
+</body>
+</html>
